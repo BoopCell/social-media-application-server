1be6a3b9af898cfaea6b2896b60f297fe2a54ba9
BoopCell <marcus.w.almqvist@gmail.com>
2023-05-27 22:21:22 +0200

    Posts, likes, users

--- server/index.js
+++ server/index.js
@@ -6,8 +6,17 @@ import multer from 'multer';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import postsRoutes from './routes/posts.js';
+import { createPost } from './controllers/posts.js';
+import { register } from './controllers/auth.js';
+import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import { users, posts } from './data/index.js';
+import Post from './models/Post.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +43,17 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
+
+/* ROUTES */
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postsRoutes);
+
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
 	.connect(process.env.MONGO_URL, {
@@ -41,8 +61,11 @@ mongoose
 		useUnifiedTopology: true,
 	})
 	.then(() =>
-		app.listen(PORT, () =>
-			console.log(`Server is listening on port: ${process.env.PORT}`),
-		),
+		app.listen(PORT, () => {
+			console.log(`Server is listening on port: ${process.env.PORT}`);
+			/* Insert fake data */
+			// User.insertMany(users);
+			// Post.insertMany(posts);
+		}),
 	)
 	.catch((error) => console.log(`${error} did not connect`));
